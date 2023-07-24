068960e2f98fe77dabdbc5651b88cf9f91aa551c
Mayank Ghanshala <mayank.ghanshala@jungleegames.com>
2023-07-24 18:01:03 +0530

    Torus

--- Assets/Basic/Mathematical Surfaces/Scripts/FunctionLibrary.cs
+++ Assets/Basic/Mathematical Surfaces/Scripts/FunctionLibrary.cs
@@ -1,35 +1,71 @@
+using UnityEngine;
 using static UnityEngine.Mathf;
 namespace CatLikeCoding.Basics
 {
     public class FunctionLibrary
     {
         #region Delegate
-        public delegate float Function(float x, float z, float t);
+        public delegate Vector3 Function(float u, float v, float t);
         #endregion
 
         #region Private Variables
-        static Function[] mFunction = { Wave, MultiWave, Ripple };
+        static Function[] mFunction = { Wave, MultiWave, Ripple, Sphere, Torus };
         #endregion
 
         #region Static Functions
-        public static float Wave(float x, float z, float t)
+        public static Vector3 Wave(float u, float v, float t)
         {
-            return Sin(PI * (x + z + t));
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (u + v + t));
+            p.z = v;
+            return p;
         }
 
-        public static float MultiWave(float x, float z, float t)
+        public static Vector3 MultiWave(float u, float v, float t)
         {
-            float y = Sin(PI * (x + .5f * t));
-            y += .5f * Sin(PI * 2f * (z + t)) * (2f / 3);
-            y += Sin(PI * (x + z + 0.25f * t));
-            return y * (1f / 2.5f);
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (u + .5f * t));
+            p.y += .5f * Sin(PI * 2f * (v + t)) * (2f / 3);
+            p.y += Sin(PI * (u + v + 0.25f * t));
+            p.y *= (1 / 2.5f);
+            p.z = v;
+            return p;
         }
 
-        public static float Ripple(float x, float z, float t)
+        public static Vector3 Ripple(float u, float v, float t)
         {
-            float d = Sqrt(x * x + z * z);
-            float y = Sin(PI * (4f * d - t));
-            return y / (1f + 10f * d);
+            Vector3 p;
+            p.x = u;
+            float d = Sqrt(u * u + v * v);
+            p.y = Sin(PI * (4f * d - t));
+            p.y /= (1f + 10f * d);
+            p.z = v;
+            return p;
+        }
+
+        public static Vector3 Sphere(float u, float v, float t)
+        {
+            float r = 0.5f + 0.5f * Sin(PI * t);
+            float s = r * Cos(0.5f * PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r * Sin(0.5f * PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+        }
+
+        public static Vector3 Torus(float u, float v, float t)
+        {
+            float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+            float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+            float s = r1 + r2 * Cos(PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r2 * Sin(PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
         }
 
         public static Function GetFunction(FunctionName functionName)
@@ -44,7 +80,9 @@ namespace CatLikeCoding.Basics
     {
         Wave,
         MultiWave,
-        Ripple
+        Ripple,
+        Sphere,
+        Torus
     }
     #endregion
 }
--- Assets/Basic/Mathematical Surfaces/Scripts/Graph3d.cs
+++ Assets/Basic/Mathematical Surfaces/Scripts/Graph3d.cs
@@ -12,41 +12,39 @@ namespace CatLikeCoding.Basics
         #endregion
 
         #region Private Variables
-        Vector3 mPos = Vector3.zero;
         Transform[] mCubeTransformArray;
+        float mStep;
         #endregion
 
         #region Unity CallBacks
         void Awake()
         {
-            float factor = 2f / _Resolution;
-            Vector3 scale = Vector3.one * factor;
+            mStep = 2f / _Resolution;
+            Vector3 scale = Vector3.one * mStep;
             mCubeTransformArray = new Transform[_Resolution * _Resolution];
-            for (int i = 0, x = 0, z = 0; i < mCubeTransformArray.Length; ++i, ++x)
+            for (int i = 0; i < mCubeTransformArray.Length; ++i)
             {
-                if (x == _Resolution)
-                {
-                    x = 0;
-                    ++z;
-                }
                 var cubeTransform = mCubeTransformArray[i] = Instantiate<GameObject>(_CubeObj, transform).transform;
                 cubeTransform.name = $"{i}";
-                mPos.x = (x + .5f) * factor - 1;
-                mPos.z = (z + .5f) * factor - 1;
-                cubeTransform.position = mPos;
                 cubeTransform.localScale = scale;
             }
         }
 
         void Update()
         {
-            float time = Time.time;
             Function function = FunctionLibrary.GetFunction(_FunctionName);
-            for (int i = 0; i < mCubeTransformArray.Length; ++i)
+            float time = Time.time;
+            float v = .5f * mStep - 1;
+            for (int i = 0, x = 0, z = 0; i < mCubeTransformArray.Length; ++i, ++x)
             {
-                mPos = mCubeTransformArray[i].position;
-                mPos.y = function(mPos.x, mPos.z, time);
-                mCubeTransformArray[i].position = mPos;
+                if (x == _Resolution)
+                {
+                    x = 0;
+                    ++z;
+                    v = (z + .5f) * mStep - 1;
+                }
+                float u = (x + .5f) * mStep - 1;
+                mCubeTransformArray[i].position = function(u, v, time);
             }
         }
         #endregion
